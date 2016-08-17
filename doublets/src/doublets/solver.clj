8dd31559cccb7440aa2335f07937d5724d41be78
Antti-Ville Jansson <janssonav@gmail.com>
2016-08-18 00:48:44 +0300

    Doublets

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -7,5 +7,35 @@
                (slurp)
                (read-string)))
 
+(defn linked? [word1 word2]
+  (let [equal-length (->> [word1 word2] (map count) (reduce =))
+        one-difference (= 1 (->> (map = word1 word2) (filter false?) (count)))]
+    (and equal-length one-difference)))
+
+(defn word-entry [word]
+  { :word word
+    :path [word]
+    :words-seen #{word} })
+
+(defn get-linked-words [word]
+  (->> words
+       (filter #(linked? (:word word) %))
+       (filter #(not (contains? (:words-seen word) (:word %))))
+       (map #(into {}
+                   [[:word %]
+                    [:path (conj (:path word) %)]
+                    [:words-seen (conj (:words-seen word) %)]]))))
+
+(defn get-links [wds target]
+  (let [linked-words (mapcat get-linked-words wds)
+        target-found (some #(when (= target (:word %)) %) linked-words)]
+    (cond
+     target-found target-found
+     (empty? linked-words) nil
+     :else (recur linked-words target))))
+
 (defn doublets [word1 word2]
-  "make me work")
+  (let [result (get-links [(word-entry word1)] word2)]
+    (if (nil? result)
+      []
+      (:path result))))
